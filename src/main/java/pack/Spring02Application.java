da6640c9b60a6a6bdc89afc18fa99edd94a4462e
tgv2013kr <tgv2013@naver.com>
2025-08-05 17:15:21 +0900

    1715

--- src/main/java/pack/Spring02Application.java
+++ src/main/java/pack/Spring02Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Spring02Application {
 	@GetMapping("/")
 	public String tiger() {
-		return "Spring02:1707";
+		return "Spring02:1715";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(Spring02Application.class, args);
